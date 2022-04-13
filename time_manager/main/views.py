80e37bd96c09532b45b04c0841b70ea8bbc84a85
GTPV <parangvo@gmail.com>
2022-04-13 16:33:24 +0900

    initial commit

--- time_manager/main/views.py
+++ time_manager/main/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-    return HttpResponse("<h1>this is main page(time_manager/main/views.py)</h1><br></br><a href='/time_manage/'>Time manage</a>")
+    return HttpResponse("<h1>this is main page(time_manager/main/views.py)</h1><br></br><a href='/time_manage/'>Time manage</a><br></br><a href='/admin'>Admin</a>")
