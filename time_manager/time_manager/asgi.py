48dd317ac43eb2780515cb81113eab5ebbd70967
GTPV <parangvo@gmail.com>
2022-04-23 23:53:13 +0900

    websocket router added

--- time_manager/time_manager/asgi.py
+++ time_manager/time_manager/asgi.py
@@ -9,11 +9,18 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from tv_display import routing as tv_display_routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'time_manager.settings')
 
 application = ProtocolTypeRouter({
-    'http':get_asgi_application()
+    'http':get_asgi_application(),
+    "websocket":AuthMiddlewareStack(
+        URLRouter(
+            tv_display_routing.websocket_patterns
+        )
+    ),
 })
