31c6091df0eccd7fc62852506a3754fe21d2624e
GTPV <parangvo@gmail.com>
2022-04-26 11:14:50 +0900

    update music on tv with websocket success

--- time_manager/tv_display/views.py
+++ time_manager/tv_display/views.py
@@ -1,6 +1,9 @@
+from channels.layers import get_channel_layer
 from django.shortcuts import render
 from .consumers import TvConsumer
 from time_manage.models import *
+from asgiref.sync import async_to_sync
+from django.conf import settings
 
 # Create your views here.
 def index(response):
@@ -10,16 +13,17 @@ def update(response, _u_i, _music_id):
     print('tv_display.views.update called!!')
     _music_info = Music.objects.get(id=_music_id)
     _music_info_dict = {
-                'music_composer': _music_info.music_composer,
-                'music_title': _music_info.music_title,
-                'music_orchestra': _music_info.music_orchestra,
-                'music_conductor': _music_info.music_conductor,
+                "music_composer": _music_info.music_composer,
+                "music_title": _music_info.music_title,
+                "music_orchestra": _music_info.music_orchestra,
+                "music_conductor": _music_info.music_conductor,
     }
     print('_music_info_dict : ' + str(_music_info_dict))
-    _tv_consumer = TvConsumer()
-    _tv_consumer.update_music( 
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        settings.STREAM_SOCKET_GROUP_NAME,
         {
-            'type': 'update_music',
-            'data': _music_info_dict,
-        }
+            'type' : 'update_music',
+            'data' : _music_info_dict,
+        },
     )
