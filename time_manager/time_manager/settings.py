1bf388a4a62ed7d9411fae90b12bd4f0a5720b62
GTPV <parangvo@gmail.com>
2022-05-17 13:54:11 +0900

    main page design

--- time_manager/time_manager/settings.py
+++ time_manager/time_manager/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collect/')
 STATICFILES_DIRS = (
     BASE_DIR / 'static/',
 )
