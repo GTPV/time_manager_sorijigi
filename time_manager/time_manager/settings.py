a23c0789ee98dbaf68e8274392855ec07de6c88a
GTPV <parangvo@gmail.com>
2022-07-19 23:10:43 +0900

    static url changed

--- time_manager/time_manager/settings.py
+++ time_manager/time_manager/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static_collect/'
 STATIC_ROOT = 'static_collect/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static/',
